f24822c5225363a49d1b884e7f062478bb581a34
KhushiT-aptus <khushi.thacker@aptusdatalabs.com>
2025-08-12 22:26:54 +0530

    Update build_and_push.sh

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -4,8 +4,9 @@ IMAGE_TAG="$1"
 REGISTRY="$2"
 PASSWORD="$3"
 USERNAME="$4"
+DOCKER_REGISTRY = "info@aptusdatalabs.com"
 
 docker build -t "$IMAGE_TAG" .
-echo "$PASSWORD" | docker login "$REGISTRY" -u "$USERNAME" --password-stdin
+echo "$PASSWORD" | docker login "DOCKER_REGISTRY" -u "$USERNAME" --password-stdin
 docker push "$IMAGE_TAG"
 docker logout "$REGISTRY"
