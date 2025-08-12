36025ceca18db6d1f335c3fc15c0bb31656eb993
KhushiT-aptus <khushi.thacker@aptusdatalabs.com>
2025-08-12 19:49:20 +0530

    changes

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ app = FastAPI(title="Demo FastAPI App", version="1.0.0")
 @app.get("/")
 def root():
     print("root")
-    return {"message": "Hello from PIE application"}
+    return {"message": "Hlo from PIE application"}
